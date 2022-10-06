dafd68045114225435f6e51fdc3e6c6cbba87771
Young998904 <107593220+Young998904@users.noreply.github.com>
2022-10-06 17:33:17 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 204 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    public int fibo(int one, int two) {
+        return one + two;
+    }
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
+        Main m = new Main();
+
+        // 0 이나 1 일 때
         if (N == 0 || N == 1) {
             System.out.println(N);
             return;
@@ -16,12 +22,9 @@ public class Main {
 
         for (int i=2; i<=N; i++) {
             newOne = two;
-            two = fibo(one, two);
+            two = m.fibo(one, two);
             one = newOne;
         }
         System.out.println(two);
-    }
-    public static int fibo(int one, int two) {
-        return one + two;
     }
 }
