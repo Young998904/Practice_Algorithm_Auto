3f6a6c6394908b796c323ace9d1f015a671c2989
Young998904 <107593220+Young998904@users.noreply.github.com>
2022-10-08 15:23:18 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 508 ms, Memory: 25752 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,25 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static int recursion(String s, int l, int r, int count){
-        count++;
-        if(l >= r) { System.out.print("1 "); return count; }
-        else if(s.charAt(l) != s.charAt(r)) { System.out.print("0 "); return count; }
-        else return recursion(s, l+1, r-1, count);
+public class Main {
+    static int cnt= 0;
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
     }
 
-    public static int isPalindrome(String s, int count){
-        return recursion(s, 0, s.length()-1, count);
-    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        for(int i = 0; i < t; i++) {
-            int count = 0;
-            String s = sc.nextLine();
-            System.out.print(isPalindrome(s, count));
-            System.out.println();
+        int N =Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=N; i++) {
+            String str = br.readLine();
+
+            int p = isPalindrome(str);
+
+            System.out.printf("%d %d\n", p, cnt);
+            cnt = 0;
         }
     }
 }
