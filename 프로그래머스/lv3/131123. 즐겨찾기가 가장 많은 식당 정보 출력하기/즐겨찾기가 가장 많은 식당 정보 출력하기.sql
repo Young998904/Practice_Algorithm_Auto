ec7b31f071fd6b5d0f2ca091aa1cfaf0bfd76b53
Young998904 <107593220+Young998904@users.noreply.github.com>
2022-10-24 17:54:11 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -6,10 +6,33 @@
 # ) AS RANKING
 # WHERE RANKING.RN <= 3
 
-SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
-FROM (
-    SELECT *, RANK() OVER (PARTITION BY RI.FOOD_TYPE ORDER BY RI.FAVORITES DESC) AS RRI
-    FROM REST_INFO AS RI
-) AS RANKING
-WHERE RANKING.RRI <= 1
+# SELECT FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+# FROM (
+#     SELECT *, RANK() OVER (PARTITION BY RI.FOOD_TYPE ORDER BY RI.FAVORITES DESC) AS RRI
+#     FROM REST_INFO AS RI
+# ) AS RANKING
+# WHERE RANKING.RRI <= 1
+# ORDER BY FOOD_TYPE DESC;
+
+# select ri.food_type, ri.rest_id, ri.rest_name, ri.favorites
+# from rest_info as ri
+# join (select food_type, max(favorites) as 'maxfavor'
+#       from rest_info
+#       group by food_type
+#      ) as mr
+# on ri.food_type = mr.food_type and ri.favorites = mr.maxfavor
+# order by ri.food_type desc
+
+# select food_type, max(favorites) as 'maxfavor'
+# from rest_info
+# group by food_type;
+
+SELECT RI.FOOD_TYPE, RI.REST_ID, RI.REST_NAME, RI.FAVORITES
+FROM REST_INFO AS RI
+INNER JOIN (
+    SELECT FOOD_TYPE, MAX(FAVORITES) AS MAXLITE
+    FROM REST_INFO
+    GROUP BY FOOD_TYPE
+) AS RM
+ON RI.FOOD_TYPE = RM.FOOD_TYPE AND RI.FAVORITES = RM.MAXLITE
 ORDER BY FOOD_TYPE DESC;
