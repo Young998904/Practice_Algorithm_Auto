e27248087fb6c391048b24402377d43c20a4a2f6
Young998904 <107593220+Young998904@users.noreply.github.com>
2022-10-28 17:15:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,9 +2,14 @@ import static java.lang.Math.*;
 
 class Solution {
     public long solution(long n) {
-        int a = (int) floor(sqrt(n));
-        int b = (int) ceil(sqrt(n));
+//         double a = floor(sqrt(n));
+//         double b = ceil(sqrt(n));
         
-        return a == b ? (long) pow(a+1, 2) : -1;
+//         return a == b ? (long) pow(a+1, 2) : -1;
+        long a = (long) sqrt(n);
+        if ((long) pow(a, 2) == n) {
+            return (long) pow(a+1, 2);
+        }
+        return -1;
     }
 }
