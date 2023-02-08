cef64df10d946d7f2b1aa8f3c6a5ba3333d4c14c
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2023-02-08 10:24:08 +0900

    [level 2] Title: 더 맵게, Time: 1634.06 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -11,6 +11,8 @@ class Solution {
             minHeap.add(scoville[i]);
         }
         
+        // 반복문 조건 (1) : 최소값을 가진 원소가 K 값보다 작다.
+        // 반복문 조건 (2) : Heap 의 원소가 1개 밖에 없다.
         while (minHeap.peek() < K && minHeap.size() != 1) {
             int top = minHeap.poll();
             int next = minHeap.poll();
@@ -19,6 +21,7 @@ class Solution {
             answer++;
         }
         
+        // 반복문을 다 돌았는데도 최소 노드 값이 K 보다 작다면 만들 수 없는 경우 의미
         if (minHeap.peek() < K) {
             return -1;
         }
@@ -27,6 +30,5 @@ class Solution {
     }
 }
 /*
-① 기본 자료구조 : (우선순위 큐) 최소힙
-②
+기본 자료구조 : (우선순위 큐) 최소힙
 */
