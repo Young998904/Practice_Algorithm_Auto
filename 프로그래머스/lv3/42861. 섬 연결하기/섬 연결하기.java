10d6e091a9885b63f99aca4d4260a1afc70876ad
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2023-07-20 15:33:56 +0900

    [level 3] Title: 섬 연결하기, Time: 0.56 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,97 +1,71 @@
-import java.util.Queue;
-import java.util.PriorityQueue;
-import java.util.ArrayList;
 
-class Node implements Comparable<Node> {
-    public int start;
-    public int end;
-    public int cost;
+/*
+크루스칼 알고리즘
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Node implements Comparable<Node> { // 노드 클래스 선언
+    int indexA, indexB, cost;
     
-    public Node (int start, int end, int cost) {
-        this.start = start;
-        this.end = end;
+    public Node (int indexA, int indexB, int cost) {
+        this.indexA = indexA;
+        this.indexB = indexB;
         this.cost = cost;
     }
     
     @Override
-    public int compareTo(Node o) {
-        return Integer.compare(o.cost, this.cost); // 내림차순
+    public int compareTo (Node other) {
+        if (this.cost < other.cost) return -1;
+        return 1;
     }
 }
+
 class Solution {
-    public static boolean[][] graph;
-    public static boolean[] visited;
-    public static int cnt = 1;
-    public static boolean done = false;
+    // root 노드 값을 담는 배열
+    public static int[] parent;
+    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        visited = new boolean[n];
-        
-        // (1) 입력값 담기
-        Queue<Node> pq = new PriorityQueue<>();
-        
-        graph = new boolean[n][n];
-
-        for (int[] cost : costs) {
-            // 우선순위 큐에 Node 담기
-            pq.offer(new Node(cost[0], cost[1], cost[2]));
-            // 인접 행렬에 연결 반영
-            graph[cost[0]][cost[1]] = true;
-            graph[cost[1]][cost[0]] = true;
+        // parent 배열 초기화
+        parent = new int[n];
+        for (int i=0; i<n; i++) {
+            parent[i] = i;
         }
-            
-        // (2) Greedy 적용하면서 전체 탐색
-        while (!pq.isEmpty()) {
-            Node node = pq.poll(); // 가장 비용이 큰 노드 [2,3,8]
-            
-            // 연결 끊기
-            graph[node.start][node.end] = false;
-            graph[node.end][node.start] = false;
-            
-            // 연결 가능 탐색
-            visited[0] = true;
-            
-            if (isLinked(0, n)) { // 경우 1 : 연결 가능하면 없어도 가능하므로 answer 에 값 추가 안함
-                reset(n);
-                continue;
+        
+        // 입력값 처리
+        ArrayList<Node> vertexList = new ArrayList<>();
+        for (int[] cost : costs) {
+            vertexList.add(new Node(cost[0], cost[1], cost[2]));
+        }
+        // 정렬
+        Collections.sort(vertexList);
+        
+        for (int i=0; i<vertexList.size(); i++) {
+            int from = vertexList.get(i).indexA;
+            int to = vertexList.get(i).indexB;
+            int cost = vertexList.get(i).cost;
+            // 루트 원소가 같지 않다면 사이클이 발생하지 않는 것이므로 묶음처리
+            if (findParent(from) != findParent(to)) {
+                union(from, to);
+                answer += cost;
             }
-            
-            // 경우2 : 연결 불가능하면 필요한 값이므로 answer 에 값 추가
-            reset(n);
-            
-            // 연결 원상복구
-            graph[node.start][node.end] = true;
-            graph[node.end][node.start] = true;
-
-            answer += node.cost;
         }
         
         return answer;
     }
     
-    // 연결 탐색 : DFS
-    public static boolean isLinked(int start, int n) {
-        if (done) return true;
-        
-        for (int i=0; i<n; i++) {
-            if (start == i || visited[i]) continue;
-            
-            if (graph[start][i]) {
-                visited[i] = true;
-                if (++cnt == n) done = true;
-                isLinked(i, n);
-            }
-        }
-        
-        if (done) return true;
-        
-        return false;
+    public static int findParent(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = findParent(parent[x]);
     }
     
-    // 연결 탐색 후 초기화
-    public static void reset(int n) {
-        cnt = 1;
-        done = false;
-        visited = new boolean[n];
+    public static void union(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+        
+        if (a < b) parent[b] = a;
+        else parent[a] = b;
     }
 }
