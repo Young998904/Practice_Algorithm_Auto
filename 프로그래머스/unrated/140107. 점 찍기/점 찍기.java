09e4cad519da24f03225b75bb7fe73de9aef687e
Cho Ah Young <107593220+Young998904@users.noreply.github.com>
2023-08-04 21:37:50 +0900

    [unrated] Title: 점 찍기, Time: 0.40 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.java
@@ -10,14 +10,7 @@ class Solution {
         double max = (double) d/k;
         
         for (long a=0; a <=max; a++) {
-//             for (long b=start; b >=0; b--) {
-                
-//                 if (Math.sqrt(a*a + b*b) > max) continue;
-                
-//                 answer += b+1;
-                
-//                 break;
-//             }
+            
             answer += Math.floor(Math.sqrt(max*max-a*a))+1;
             
         }
